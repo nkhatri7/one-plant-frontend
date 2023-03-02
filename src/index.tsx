0580ebbae2244758e205b543a70c3238948d98df
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-03-02 14:45:32 +1100

    :recycle: Remove goerli ETH for prod

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { WagmiConfig, createClient } from 'wagmi';
-import { mainnet, goerli } from 'wagmi/chains';
+import { Chain, goerli, mainnet } from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 import App from './App';
 import './index.scss';
 
 // Create the client using ConnectKit's `getDefaultClient`
 const ALCHEMY_ID = import.meta.env.VITE_ALCHEMY_KEY;
+const ENV = import.meta.env.VITE_ENV;
+let chains: Chain[];
+// Dev environment will allow goerli ETH whilst prod will only allow mainnet
+if (ENV === 'dev') {
+    chains = [goerli, mainnet];
+} else {
+    chains = [mainnet];
+}
 const client = createClient(
     getDefaultClient({
         appName: 'One Plant',
         alchemyId: ALCHEMY_ID,
-        chains: [mainnet, goerli],
+        chains: chains,
     })
 );
 
